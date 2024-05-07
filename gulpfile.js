896448b4a7f40a67e9c0e772f213d39897d46a53
luis_mazo <luis.mzo@hotmail.com>
2024-05-07 01:22:44 -0300

    Se agrego compilación y watch con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
+import * as darSass from "sass"
+import gulpSass from "gulp-sass"
+import {src, dest, watch} from "gulp"
+
+const sass = gulpSass(darSass) 
+
 //Con export ya puede usarse desde el package.json
-export function hola( done ){
-    console.log("Hola desde gulp")
+export function css (done){
+    src("src/scss/app.scss") //Buscamos el archivo a compilar
+        .pipe( sass()) //Ejecuta esta función con una determinada tarea con el archivo. Con este pipe indicamos que compile. 
+        .pipe( dest("build/css")) //Establecemos el destino del archivo compilado. 
     done()
+}
+
+export function dev(){
+    watch("src/scss/app.scss", css)
 }
