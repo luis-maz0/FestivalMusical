62f03282dfb1e3031874ba8d4d58f7727efe6254
luis_mazo <luis.mzo@hotmail.com>
2024-05-13 18:10:23 -0300

    estilos globales & archivos base agregados

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,11 @@ const sass = gulpSass(darSass)
 //Con export ya puede usarse desde el package.json
 export function css (done){
     src("src/scss/app.scss") //Buscamos el archivo a compilar
-        .pipe( sass()) //Ejecuta esta función con una determinada tarea con el archivo. Con este pipe indicamos que compile. 
+        .pipe( sass().on("error", sass.logError)) //Ejecuta esta función con una determinada tarea con el archivo. Con este pipe indicamos que compile. En caso de que ocurra un error lo atrapamos y visualizamos con on. 
         .pipe( dest("build/css")) //Establecemos el destino del archivo compilado. 
     done()
 }
 
 export function dev(){
-    watch("src/scss/app.scss", css)
+    watch("src/scss/**/*.scss", css)
 }
