8970a987ad1d5d5c1e3f4d84134e8b315f7aad6f
luis-maz0 <luis.mzo@hotmail.com>
2024-05-23 13:51:12 -0300

    modal agregado con js (refactorizar)

--- build/js/app.js
+++ build/js/app.js
@@ -8,6 +8,42 @@ function crearGaleria(){
         img.className = "galeria-img"
         img.src = `src/img/galeria${i}.jpg`; // Ajusta la ruta y nombre de las imágenes según tu caso
         img.alt = `imagen galeria ${i}`;
+
+        //EVENT HANDLER
+        img.onclick = function(){
+            mostrarImagen(i)
+        }
         galeria.appendChild(img);
     }
+}
+function mostrarImagen(indiceImg){
+    const img = document.createElement("IMG");
+    img.className = "modal-img"
+    img.src = `src/img/galeria${indiceImg}.jpg`;
+    img.alt = `imagen galeria ${indiceImg}`;
+
+    //General fondo o modal de img
+    const modal = document.createElement("DIV");
+    modal.classList.add("modal");
+    modal.appendChild(img);
+    
+    //Agregar al body
+    const body = document.querySelector("body");
+    body.classList.add("overflow-hidden");
+    body.appendChild(modal);
+    
+    //Handler para sacar modal
+    modal.onclick = function(){
+        removerModal();
+        body.classList.remove("overflow-hidden");
+    }
+}
+
+function removerModal(){
+    const modal = document.querySelector(".modal");
+    modal.classList.add ("modal-fadeOut");
+    //Retrasamos el tiempo para la ejecución del bloque del setTimeOut. 
+    setTimeout(() => {
+        modal.remove();
+    }, 500);
 }
