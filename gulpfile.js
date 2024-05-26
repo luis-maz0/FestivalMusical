d9fae6a4b26bbc08bbcfc6d67fcbb7cd684f1e26
luis_mazo <luis.mzo@hotmail.com>
2024-05-26 03:32:57 -0300

    minificacion de archivos completado

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 import * as darSass from "sass"
 import gulpSass from "gulp-sass"
 import {src, dest, watch, series} from "gulp"
+import terser from "gulp-terser"
 
 const sass = gulpSass(darSass) 
+
 
 //Con export ya puede usarse desde el package.json
 export function css (done){
     src("src/scss/app.scss",{sourcemaps: true}) //Buscamos el archivo a compilar
-        .pipe( sass().on("error", sass.logError)) //Ejecuta esta función con una determinada tarea con el archivo. Con este pipe indicamos que compile. En caso de que ocurra un error lo atrapamos y visualizamos con on. 
+        //Pasamos como argumento de la función sass outputStype para minificar la hoja de estilos(ganamos bytes)
+        .pipe( sass({ outputStyle: "compressed"}).on("error", sass.logError)) //Ejecuta esta función con una determinada tarea con el archivo. Con este pipe indicamos que compile. En caso de que ocurra un error lo atrapamos y visualizamos con on. 
         .pipe( dest("build/css",{sourcemaps: true})) //Establecemos el destino del archivo compilado. 
         //Con sourcemaps nos mostrará la linea de código NO del css sino del scss
         //(Visto desde devtools)
@@ -17,6 +20,7 @@ export function css (done){
 //Genera una función que mande nuestro js al directorio build.
 export function js( done ){
     src("src/js/app.js")
+        .pipe(terser())
         .pipe(dest("build/js"))
     done()
 }
